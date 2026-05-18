ff62578b47982672541fe5d831a25a56e5d5d5ae
gjdbradford <gjdbradford@gmail.com>
2026-05-18 20:00:31 +0200

    feat(portal): add PayFast provider stub with typed 503 error

--- lib/payment/providers/payfast.ts
+++ lib/payment/providers/payfast.ts
@@ -1,6 +1,16 @@
-import type { PaymentProvider } from '../types'
+import type { PaymentProvider, CheckoutParams, CheckoutResult } from '../types'
+
+export class PayFastNotImplementedError extends Error {
+  readonly statusCode = 503
+  constructor() {
+    super('PayFast payment integration is not yet available. Please contact support.')
+  }
+}
 
 export const payfastProvider: PaymentProvider = {
   name: 'payfast',
-  async createCheckoutSession() { throw new Error('not implemented') },
+
+  async createCheckoutSession(_params: CheckoutParams): Promise<CheckoutResult> {
+    throw new PayFastNotImplementedError()
+  },
 }
